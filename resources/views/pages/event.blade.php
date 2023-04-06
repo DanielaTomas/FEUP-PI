ea6f0b52b816b3b17e609ef4d8c4aeefd3bda4ea
Daniela Tomás <daniela.stomas02@gmail.com>
2023-04-06 11:30:13 +0100

    add event page

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<h1> GOOD LUCK FRONT-ENDERS</h1>
+
+<div class="p-5 m-5 bg-secondary rounded min-height">
+    <div class="d-flex justify content-center link-light">
+        <h3> {{$event->eventname}} </h3>
+    </div>
+
+    <div class="py-5">
+        <div class="card" style="width: 30rem; float:left; margin-right: 5rem;">
+            <div class="card-body">
+                <p class="card-text"><b>Start Date: </b>{{$event->startdate}}</p>
+                <p class="card-text"><b>End Date: </b>{{$event->enddate}}</p>
+                <p class="card-text"><b>Address: </b>{{$event->address}}</p>
+                <p class="card-text"><b>Contact Person: </b>{{$event->contactperson}}</p>
+                <p class="card-text"><b>Email: </b>{{$event->email}}</p>
+            </div>
+        </div>
+
+        <div class="mx-5">
+            <h5>Description</h5>
+            <p>{{$event->description}}</p>
+        </div>
+    </div>
+</div>
+
 @endsection
