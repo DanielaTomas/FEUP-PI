09c19c1af1945a0e7e5ba1b79ea5f29cb02f1715
Miguel Tavares <up202002811@fe.up.pt>
2023-04-08 18:41:23 +0100

    New Sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,12 @@
           </svg>
         </a>
 
+        <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" style="background-color:transparent">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="White" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </button>
+
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" class="nav-link px-2 link-secondary">My Requests</a></li>
           <li><a href="#" class="nav-link px-2 link-secondary">Abouts US</a></li>
@@ -68,6 +74,8 @@
 
   </button>
 
+  
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -97,15 +105,16 @@
   </div>
 </nav>
 
-<div class ="d-flex flex-row" id="navbarSupportedContent">
-  <div class="flex-grow-0 p-3 bg-dark flex-direction:column min-vh-100" id="navbarSupportedContent" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center pb-3 mb-3 nav-link text-decoration-none border-bottom">
-      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-5 fw-semibold text-light">Yellow Pages</span>
-    </a>
+<div class="offcanvas offcanvas-start bg-dark" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header justify-content-center">
+    <h5 class="offcanvas-title text-white" id="offcanvasExampleLabel">Paginas Amarelas</h5>
+      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <hr id="hrSide">
+  <div class="offcanvas-body text-white">
     <ul class="list-unstyled ps-0">
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
           Home ↓
         </button>
         <div class="collapse" id="home-collapse">
@@ -117,7 +126,7 @@
         </div>
       </li>
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
           Event Categories ↓
         </button>
         <div class="collapse" id="dashboard-collapse">
@@ -130,7 +139,7 @@
         </div>
       </li>
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
           Category 1 ↓
         </button>
         <div class="collapse" id="orders-collapse">
@@ -143,7 +152,7 @@
         </div>
       </li>
       <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+        <button id="sideButton"class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
           Category 2 ↓
         </button>
         <div class="collapse" id="account-collapse">
@@ -157,6 +166,7 @@
       </li>
     </ul>
   </div>
+</div>
 
   @yield('content')
 
