dba821ccb251eec230298da036487a836ae8f624
Miguel Tavares <up202002811@fe.up.pt>
2023-04-20 17:29:16 +0100

    Holiday API added

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,75 @@
 //import './bootstrap';
 
-const today = new Date();
+const currentYear = new Date().getFullYear();
+const url = `https://date.nager.at/api/v3/PublicHolidays/${currentYear}/PT`;
 
-const xmas = new Date('2023-12-01').getMonth();
-const easter = new Date('2023-04-01').getMonth();
-const halloween = new Date('2023-10-01').getMonth();
-const newYear = new Date('2023-01-01').getMonth();
+function processHolidays(holidays) {
+    const today = new Date();
+    const homeIcon = document.querySelector('#home');
 
-const homeIcon = document.querySelector('#home');
+    const xmas = new Date(holidays['Christmas Day']);
+    const xmasMonth = xmas.getMonth() + 1;
+ 
+    const easter = new Date(holidays['Easter Sunday']);
+    const easterMonth = easter.getMonth() + 1;
 
-    /*check if december  */
-    if(today.getMonth() === xmas){
-        const xmasIcon =document.querySelector('#xmas');
-        xmasIcon.style.display = "block";
-        homeIcon.style.display = "none";
+    /*holiday wont ever change so its fine, not in the API too*/
+    const spookyMonth = new Date('2023-10-31');
+    const halloween = spookyMonth.getMonth() + 1;
+    
+    /*new Year */
+    const newYear = new Date(holidays["New Year's Day"]);
+    const newYearMonth = newYear.getMonth() + 1;
+
+    switch(today.getMonth() + 1){
+        /*check if december(12), make it last december */
+        case xmasMonth:
+            const xmasIcon =document.querySelector('#xmas');
+            xmasIcon.style.display = "block";
+            homeIcon.style.display = "none";
+            break;
+        /*check if april(4)*/
+        case easterMonth:
+            if(today.getDate() > easter.getDate()) break;
+            console.log("april");
+            const easterIcon =document.querySelector('#easter');
+            easterIcon.style.display = "block";
+            homeIcon.style.display = "none";
+            break;
+        /*check if october, its the spooky month */
+        case spookyMonth:
+            const halloweenIcon = document.querySelector('#halloween');
+            halloweenIcon.style.display = "block";
+            homeIcon.style.display = "none";
+            break;
+        /*check if new year, lasts one week */
+        case newYearMonth:
+            if(today.getDate() >= 7) break;
+            const newYearIcon = document.querySelector('#newYear');
+            newYearIcon.style.display = "block";
+            homeIcon.style.display = "none";
+            break;
+        default:
+            console.log("no festivities");
+            break;
     }
-    /*check if april */
-    else if(today.getMonth() === easter){
-        const easterIcon =document.querySelector('#easter');
-        easterIcon.style.display = "block";
-        homeIcon.style.display = "none";
-    }
-    else if(today.getMonth() === halloween){
-        const halloweenIcon = document.querySelector('#halloween');
-        halloweenIcon.style.display = "block";
-        homeIcon.style.display = "none";
-    }
-    else if(today.getMonth() === newYear){
-        const newYearIcon = document.querySelector('#newYear');
-        newYearIcon.style.display = "block";
-        homeIcon.style.display = "none";
-    }
+}
+
+function getHolidays(callback) {
+  let holidays = {};
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(holiday => {
+        holidays[holiday.name] = holiday.date;
+      });
+      callback(holidays);
+    })
+    .catch(error => console.error(error));
+}
+
+getHolidays(processHolidays);
 
 
 const editeventform=document.querySelector('#editeventform');
