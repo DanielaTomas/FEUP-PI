621c2b4df309dd8e4c53d646e25bf7c79a74f4ff
Miguel Tavares <up202002811@fe.up.pt>
2023-05-05 14:58:45 +0100

    admin GI table

--- resources/views/pages/adminGis.blade.php
+++ resources/views/pages/adminGis.blade.php
@@ -12,28 +12,41 @@
             {{ session('success') }}
         </div>
     @endif
-
-    <form method="POST" action="{{ route('users.search') }}">
-        {{ csrf_field() }}
-        <input type="search" class="form-control my-5" name="q" placeholder="Search for a user..." aria-label="Search">
-        <button type="submit">Search</button>
-    </form>
+    
+    <div class="text-center">
+        <form method="POST" action="{{ route('users.search') }}">
+            {{ csrf_field() }}
+            <input type="search" class="form-control my-2" name="q" placeholder="Search for a user..." aria-label="Search">
+            <button class="rounded my-2" type="submit">Search</button>
+        </form>
+    </div>
     
     @isset($users)
         <ul>
-            @foreach($users as $user)
-            
-                <form method="GET" action="{{ route('users.assignRole', $user->userid) }}">
-                    <p> {{  $user->name }} </p>
-                    <select name="organicunitid">
-                        @foreach ($organicunits as $organicunit)
-                            <option value="{{ $organicunit->organicunitid }}">{{ $organicunit->name }}</option>
-                        @endforeach
-                    </select>
-                    <input type="submit" value ="Assign GI to selected unit">
-                </form>
-
-            @endforeach
+            <table class="table rounded rounded-3 overflow-hidden align-middle bg-white">
+                <thead class="bg-light">
+                    <tr>
+                    <th class="text-center">User</th>
+                    <th class="text-center">Organic Unit</th>
+                    <th class="text-center">Submit</th>
+                    </tr>
+                </thead>
+                <tbody id="CurrEventTable">
+                    @foreach($users as $user)
+                    <tr>
+                    <form method="GET" action="{{ route('users.assignRole', $user->userid) }}">
+                        <td class="text-center"> {{  $user->name }} </td>
+                        <td class="text-center"><select name="organicunitid">
+                                @foreach ($organicunits as $organicunit)
+                                    <option value="{{ $organicunit->organicunitid }}">{{ $organicunit->name }}</option>
+                                @endforeach
+                        </select></td>
+                        <td class="text-center"><input type="submit" value ="Assign GI to selected unit"></td>
+                    </form>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </ul>
     @endisset
     
