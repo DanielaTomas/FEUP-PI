99cf62ecb07d777542b770442a0f46069fb84e7b
Daniela Tomás <daniela.stomas02@gmail.com>
2023-05-24 17:35:12 +0100

    update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <div class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none" title="Home">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="White" class="bi bi-house-door-fill" viewBox="0 0 16 16" id="home">
               <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
             </svg>
@@ -53,7 +53,6 @@
             </a>
             <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
               <li><a class="dropdown-item" href="{{route('my.requests')}}">My Requests</a></li>
-              <li><a class="dropdown-item" href="{{route('my.requests')}}">My Events</a></li>
               @if(!Session::has('orig_user') && Auth::check() && Auth::user()->isAdmin())
                 <li><a class="dropdown-item" href="{{url('admin')}}">Admin Dashboard</a></li>
                 <li><a class="dropdown-item" href="/admin/user/switch/start">Switch to User View</a></li>      
@@ -93,39 +92,6 @@
           </ul>
       </div>
   @endif
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link text-light" href="{{route('my.requests')}}">My Requests<span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#">About Us</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled text-light" href="#">Contacts</a>
-      </li>
-      @if (Auth::check())
-      <li class="nav-item">
-        <a class="nav-link text-light" href="{{route('create.event')}}">Create Event Request</a>
-      </li>
-      @endif
-    </ul>
-    @if (Auth::check())
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0 text-light" type="submit"><a href="{{ route('logout') }}">Logout</a></button>
-    </form>
-    @else
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0 text-light" type="submit"><a href="{{ route('login') }}">Login</a></button>
-    </form>
-    @endif
-  </div>
-</nav>
 
 <div class="offcanvas offcanvas-start bg-dark" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header justify-content-center">
@@ -136,56 +102,89 @@
   <div class="offcanvas-body text-white">
     <ul class="list-unstyled ps-0">
       <li class="mb-1">
-        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-          Home ↓
-        </button>
-        <div class="collapse" id="home-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="nav-link rounded text-light">Overview</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Updates</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Reports</a></li>
-          </ul>
-        </div>
+        <a href="/"><button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          Home
+        </button></a>
       </li>
       <li class="mb-1">
         <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-          Event Categories ↓
+          Events ↓
         </button>
         <div class="collapse" id="dashboard-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="events" class="nav-link rounded text-light">Overview</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Weekly</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Monthly</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Annually</a></li>
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="/events" class="nav-link rounded text-light">Overview</a></li>
           </ul>
         </div>
-      </li>
-      <li class="mb-1">
-        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-          Category 1 ↓
-        </button>
-        <div class="collapse" id="orders-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="nav-link rounded text-light">New</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Processed</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Shipped</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Returned</a></li>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="/categories/events" class="nav-link rounded text-light">Categories</a></li>
           </ul>
         </div>
+        @if(Auth::check())
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="/my_requests" class="nav-link rounded text-light">My Requests</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="/create_event" class="nav-link rounded text-light">Create Event Request</a></li>
+          </ul>
+        </div>
+        @endif
       </li>
       <li class="mb-1">
         <button id="sideButton"class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          Category 2 ↓
+          Services ↓
         </button>
         <div class="collapse" id="account-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="nav-link rounded text-light">New...</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Profile</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Settings</a></li>
-            <li><a href="#" class="nav-link rounded text-light">Sign out</a></li>
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/services')}}" class="nav-link rounded text-light">Overview</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/service/1')}}" class="nav-link rounded text-light">HPC and Grid Computing</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/service/2')}}" class="nav-link rounded text-light">Virtual Machine</a></li>
           </ul>
         </div>
       </li>
+      @if(Auth::check() && Auth::user()->isAdmin() && !Session::has('orig_user'))
+      <li class="mb-1">
+        <button id="sideButton" class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          Admin Dashboard ↓
+        </button>
+        <div class="collapse" id="orders-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/admin/events')}}" class="nav-link rounded text-light">Events</a></li>
+          </ul>
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/admin/services')}}" class="nav-link rounded text-light">Services</a></li>
+          </ul>
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 mx-3 small">
+            <li><a href="{{url('/admin/gis')}}" class="nav-link rounded text-light">GIs</a></li>
+          </ul>
+        </div>
+      </li>
+      @endif
+      <ul class="d-flex list-unstyled position-absolute bottom-0 start-0 p-3">
+        <li class="nav-item bg-dark"><a href="{{url('/about')}}" class="nav-link px-2 text-light">About Us</a></li>
+        <li class="nav-item bg-dark"><a href="{{url('/faq')}}" class="nav-link px-2 text-light">FAQs</a></li>
+        <li class="nav-item bg-dark"><a href="{{url('/contacts')}}" class="nav-link px-2 text-light">Contacts</a></li>
+      </ul>
+      <ul class="list-unstyled position-absolute bottom-0 end-0 p-3">
+        <li>
+          <a href="{{url('feed')}}" class="nav-link px-2 link-light" title="RSS Feed">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-rss-fill" viewBox="0 0 16 16">
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm1.5 2.5c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1 0-2zm0 4a6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1 0-2zm.5 7a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+            </svg>
+          </a>
+        </li>
+      </ul>
     </ul>
   </div>
 </div>
@@ -194,20 +193,14 @@
 
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 bg-dark mt-auto">
     <p class="col-md-4 mb-0 text-light p-3">© 2023 Company, Inc ඞ</p>
-
-    <!--<a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>-->
-
     <ul class="nav col-md-4 justify-content-end px-3">
       <li class="nav-item bg-dark"><a href="{{url('/')}}" class="nav-link px-2 text-light">Home</a></li>
-      <li class="nav-item bg-dark"><a href="#" class="nav-link px-2 text-light">My Requests</a></li>
       <li class="nav-item bg-dark"><a href="{{url('/about')}}" class="nav-link px-2 text-light">About Us</a></li>
       <li class="nav-item bg-dark"><a href="{{url('/faq')}}" class="nav-link px-2 text-light">FAQs</a></li>
       <li class="nav-item bg-dark"><a href="{{url('/contacts')}}" class="nav-link px-2 text-light">Contacts</a></li>
       <li>
-        <a href="{{url('feed')}}" class="nav-link px-2 link-light">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-rss-fill" viewBox="0 0 16 16">
+        <a href="{{url('feed')}}" class="nav-link px-2 link-light" title="RSS Feed">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-rss-fill" viewBox="0 0 16 16">
             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm1.5 2.5c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1 0-2zm0 4a6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1 0-2zm.5 7a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
           </svg>
         </a>
