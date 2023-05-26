dc4260bda23ae5730f28594ae86f09aebb6a5bad
Daniela Tomás <daniela.stomas02@gmail.com>
2023-05-26 09:56:27 +0100

    fix vulnerability

--- routes/web.php
+++ routes/web.php
@@ -22,27 +22,27 @@ Route::get('/', 'HomeController@list')->name('home');
 // Events
 Route::get('/event/create', 'EventController@createEventForm')->name('create.event');
 Route::post('/event/create', 'EventController@createEvent')->name('create.event');
-Route::get('/event/{id}/edit', 'EventController@editEventForm')->name('edit.event');
-Route::post('/event/{id}/edit', 'EventController@editEvent')->name('edit.event');
-Route::get('/event/{id}/delete', 'EventController@deleteEvent')->name('delete.event');
+Route::get('/event/{id}/edit', 'EventController@editEventForm')->where(['id' => '[0-9]+'])->name('edit.event');
+Route::post('/event/{id}/edit', 'EventController@editEvent')->where(['id' => '[0-9]+'])->name('edit.event');
+Route::get('/event/{id}/delete', 'EventController@deleteEvent')->where(['id' => '[0-9]+'])->name('delete.event');
 Route::get('/events', 'EventController@list')->name('events'); //TODO: mudar nome de funcao?? ou de controller??
-Route::get('/event/{id}', 'EventController@show')->name('event');
-Route::get('/tags/{id}/events', 'TagController@show')->name('tags.events');
+Route::get('/event/{id}', 'EventController@show')->where(['id' => '[0-9]+'])->name('event');
+Route::get('/tags/{id}/events', 'TagController@show')->where(['id' => '[0-9]+'])->name('tags.events');
 Route::get('/categories/events', 'CategoryController@showEventCategories')->name('categories.events');
-Route::get('/organicunits/{id}/events', 'OrganicUnitController@show')->name('organics.events');
+Route::get('/organicunits/{id}/events', 'OrganicUnitController@show')->where(['id' => '[0-9]+'])->name('organics.events');
 Route::get('/events/seach', 'EventController@search')->name('search.events');
 
 // SERVICES
 
 Route::get('/services','ServiceController@list')->name('services');
 //Route::get('/service/{id}','ServiceController@createServiceForm')->name('create.service');
-Route::get('/service/{id}', 'ServiceController@show')->name('show.service');
-Route::get('/service/{id}/create', 'ServiceController@createServiceForm')->name('create.service');
+Route::get('/service/{id}', 'ServiceController@show')->where(['id' => '[0-9]+'])->name('show.service');
+Route::get('/service/{id}/create', 'ServiceController@createServiceForm')->where(['id' => '[0-9]+'])->name('create.service');
 Route::post('/create.service', 'ServiceController@createService')->name('create.service.form');
-Route::get('/delete.service/{id}', 'ServiceController@deleteService')->name('delete.service');
-Route::get('/show.service/{id}', 'ServiceController@showServiceForm')->name('show.service');
-Route::get('/edit.service/{id}', 'ServiceController@editServiceForm')->name('edit.service');
-Route::post('/edit.service/{id}', 'ServiceController@editService')->name('edit.service');
+Route::get('/delete.service/{id}', 'ServiceController@deleteService')->where(['id' => '[0-9]+'])->name('delete.service');
+Route::get('/show.service/{id}', 'ServiceController@showServiceForm')->where(['id' => '[0-9]+'])->name('show.service');
+Route::get('/edit.service/{id}', 'ServiceController@editServiceForm')->where(['id' => '[0-9]+'])->name('edit.service');
+Route::post('/edit.service/{id}', 'ServiceController@editService')->where(['id' => '[0-9]+'])->name('edit.service');
 Route::post('/new.service', 'ServiceController@createNewService')->name('new.service');
 
 
@@ -62,8 +62,8 @@ Route::get("/admin", 'AdminController@show');
 Route::get('/admin/events', 'EventControllerAdmin@show');
 Route::get('/admin/eventsCurrent', 'EventControllerAdmin@showCurrent');
 Route::get('/admin/eventsPending', 'EventControllerAdmin@showPending');
-Route::post('/requests/events/{id}/{action}', 'EventControllerAdmin@updateStatus')->name('requests.status.update')->where(['action' => '(Accepted|Rejected)']);
-Route::get("/admin/user/{id}/assign/gi", 'UserControllerAdmin@assignGI')->name('users.assignRole');
+Route::post('/requests/events/{id}/{action}', 'EventControllerAdmin@updateStatus')->where(['id' => '[0-9]+', 'action' => '(Accepted|Rejected)'])->name('requests.status.update');
+Route::get("/admin/user/{id}/assign/gi", 'UserControllerAdmin@assignGI')->where(['id' => '[0-9]+'])->name('users.assignRole');
 
 //TODO: adicionar permission checks as routes por baixo
 
@@ -77,7 +77,7 @@ Route::get('/admin/user/switch/stop','AdminController@user_switch_stop');
 //Route::get("/admin/services", 'ServiceControllerAdmin@show');
 Route::get('/admin/servicesCurrent', 'ServiceControllerAdmin@showCurrent');
 Route::get('/admin/servicesPending', 'ServiceControllerAdmin@showPending');
-Route::post('/requests/services/{id}/{action}', 'ServiceControllerAdmin@updateStatus')->name('requests.services.status.update')->where(['action' => '(Accepted|Rejected)']);
+Route::post('/requests/services/{id}/{action}', 'ServiceControllerAdmin@updateStatus')->where(['id' => '[0-9]+', 'action' => '(Accepted|Rejected)'])->name('requests.services.status.update');
 
 Route::get("/admin/services", function () {
     $organicunits = app('App\Http\Controllers\OrganicUnitController')->getOrganicUnits();
